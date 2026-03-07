acefde25e8de8ecb360de163acee3bf8e90622f6
Zabedfolio <zabedfolio@gmail.com>
2026-03-07 23:45:38 +0600

    added spinner

--- script/home.js
+++ script/home.js
@@ -3,7 +3,28 @@ let activeTab = "all"
 let issueData = [];
 
 
+// spinner
+const manageSpinner = (status)=>{
+    if(status == true){
+        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById("issue-container").classList.add("hidden");
+    }else{
+        document.getElementById("issue-container").classList.remove("hidden");
+        document.getElementById("spinner").classList.add("hidden");
+    }
+}
 
+const api = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+// console.log(api.length)
+manageSpinner(true);
+fetch(api)
+    .then(res => res.json())
+    .then(data => {
+        issuesData = data.data;
+        showIssues(issuesData);
+        manageSpinner(false);
+    })
 
 // modal
 const loadIssueDetail = async (id) => {
